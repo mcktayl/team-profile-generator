0b95cf8a446f2e5f8779d4e50a4c7d28ee00de05
McKenzie Cottrell <mckenziecottrell@gmail.com>
2022-02-28 15:09:42 -0500

    added ability to create engineer and add to array

--- index.js
+++ index.js
@@ -42,4 +42,60 @@ function createManager() {
     });
 }
 
-createManager();
+function createTeam() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'role',
+        choices: ['Engineer', 'Intern'],
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is this employee's name?",
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What is this employee's id?",
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is this employee's email?",
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: "What is this employee's GitHub username?",
+        when: (input) => input.role === 'Engineer',
+      },
+      {
+        type: 'input',
+        name: 'school',
+        message: "What is this employee's school?",
+        when: (input) => input.role === 'Intern',
+      },
+      {
+        type: 'confirm',
+        name: 'addMore',
+        message: 'Would you like to add another employee?',
+      },
+    ])
+    .then((employeeInput) => {
+      const name = employeeInput.name;
+      const id = employeeInput.id;
+      const email = employeeInput.email;
+      const github = employeeInput.github;
+      const school = employeeInput.school;
+
+      if (employeeInput.role === 'Engineer') {
+        const engineer = new Engineer(name, id, email, github);
+        teamArray.push(engineer);
+        console.log(teamArray);
+      }
+    });
+}
+
+createTeam();
