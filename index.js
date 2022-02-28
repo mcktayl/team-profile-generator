a021a927897a201f55c98b7b68c1c60ec5fd6822
McKenzie Cottrell <mckenziecottrell@gmail.com>
2022-02-28 14:30:01 -0500

    added function to create team manager and add them to array

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
+
+const teamArray = [];
+
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is the team manager's name?",
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What is the manager's employee id?",
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is the manager's email?",
+      },
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the manager's office number?",
+      },
+    ])
+    .then((managerInput) => {
+      const name = managerInput.name;
+      const id = managerInput.id;
+      const email = managerInput.email;
+      const officeNumber = managerInput.officeNumber;
+
+      const manager = new Manager(name, id, email, officeNumber);
+      teamArray.push(manager);
+      console.log(teamArray);
+    });
+}
+
+createManager();
